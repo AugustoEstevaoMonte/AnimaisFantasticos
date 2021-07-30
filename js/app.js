176406ae569fc0ef954a641f22aae31889a8329c
Augusto Estevão <augusto-estevao@hotmail.com>
2021-07-30 17:10:19 -0300

    Outro commit

--- js/app.js
+++ js/app.js
@@ -1,25 +1,32 @@
-const imgsAnimais = document.querySelectorAll("img");
-const imagemArray = Array.from(imgsAnimais);
-console.log(imagemArray);
+const menuItens = document.querySelectorAll(".menu ul li a");
 
-//SELECIONAR TODAS AS IMAGENS QUE COMEÇAM COM A PALAVRA IMAGEM
-const imgStart = document.querySelectorAll("img[src^='/img/imagem']");
-const imgArray = Array.from(imgStart);
-console.log(imgArray);
-//SELECIONAR TODAS AS IMAGENS QUE COMEÇAM COM A PALAVRA IMAGEM
+menuItens.forEach(function(item,index){item.classList.add('ativo');console.log(index);});
 
-//SELECTIONE TODOS OS LINKS INTERNOS
-const hrefAnimais = document.querySelectorAll("a[href^='#']");
-const hrefArray = Array.from(hrefAnimais);
-console.log(hrefArray);
-//SELECTIONE TODOS OS LINKS INTERNOS
+menuItens.forEach(function(item,index)
+{
+  
+});
 
-//SELECIONA APENAS 1 H2 DENTRO DE ANIMAIS DESCRICAO
-const primeiroH2 = document.querySelector(".animais-descricao h2");
-console.log(primeiroH2);
-//SELECIONA APENAS 1 H2 DENTRO DE ANIMAIS DESCRICAO
+console.log(menuItens);
 
-//SELECIONA APENAS O ÚLTIMO P  DENTRO DO SITE
-const lastP = document.querySelectorAll('p');
-console.log(lastP[--lastP.length]);
-//SELECIONA APENAS O ÚLTIMO P  DENTRO DO SITE
+const imgs = document.querySelectorAll('img');
+let i = 0;
+imgs.forEach(function(item,index)
+{
+    if(item.hasAttribute('alt'))
+    {
+        i++;
+    }
+});
+
+console.log("Imagens que possuem alt: " + i);
+
+const hrefs = document.querySelectorAll(".menu ul li a");
+
+hrefs.forEach(function(item,index){
+
+  item.setAttribute('href',"hello world.exe");
+});
+
+console.log(hrefs);
+
